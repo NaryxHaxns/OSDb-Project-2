da31760aa050e5fc4e52eb2a25d1c731fb93bc2b
Ryan Nash <nashkbry@gmail.com>
2020-06-01 18:02:19 -0700

    performer show implementation

--- controllers/performers.js
+++ controllers/performers.js
@@ -1,11 +1,34 @@
 const Performer = require('../models/performer');
 
 module.exports = {
-    index
+    index,
+    add,
+    create,
+    show
 }
 
 function index(req,res){
     Performer.find({}, function(err,performers){
-        res.render('performers', { title: 'OSDb: Online Stage Database - People', performers });
+        res.render('performers', { title: 'OSDb: Online Stage Database - Performers', performers });
+    })
+}
+
+function add(req,res){
+    res.render('performers/add', { title: 'OSDb: Online Stage Database - Add a Performer' })
+}
+
+function create(req,res){
+    const s = req.body.born;
+    req.body.born = `${s.substr(5, 2)}-${s.substr(8, 2)}-${s.substr(0, 4)}`;
+    const performer = new Performer(req.body);
+    performer.save(function(err){
+        if (err) return res.redirect('/performers/add');
+        res.redirect('/performers');
+    })
+}
+
+function show(req,res){
+    Performer.findById(req.params.id, function(err,performers){
+        res.render('performers/show', { title: 'OSDb: Online Stage Database - Performer', performers });
     })
 }
--- routes/performers.js
+++ routes/performers.js
@@ -4,4 +4,12 @@ const performersCtrl = require('../controllers/performers');
 
 router.get('/', performersCtrl.index);
 
+router.get('/add', performersCtrl.add);
+
+router.get('/:id', performersCtrl.show);
+
+router.post('/', performersCtrl.create);
+
+
+
 module.exports = router;
